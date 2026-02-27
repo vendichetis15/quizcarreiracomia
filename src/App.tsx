1e9821c0ba06df501fc3890e6aa3ec230108646d
Henrique <henriquevendichetis@gmail.com>
2026-02-27 18:03:46 -0300

    Fix Sonner import and add UTM tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
-import { Sonner } from "@/components/ui/sonner";
+import { Toaster as SonnerToaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -28,7 +28,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
+      <SonnerToaster />
       <BrowserRouter>
         <AppContent />
       </BrowserRouter>
